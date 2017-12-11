38bbaf2d95c0998df3132828278fce19075e4ec4
John Murray <jmurrayufo@gmail.com>
2017-12-11 04:49:02 -0700

    Misused variable name

--- InGenStation/code/CustomLogging/GlobalLog.py
+++ InGenStation/code/CustomLogging/GlobalLog.py
@@ -25,8 +25,8 @@ class Log:
         self._log.addHandler(ch)
 
         self._metric_log = logging.getLogger(_type)
-        ch = logging.StreamHandler(stream=None)
-        self._metric_log.addHandler(ch)
+        ch2 = logging.StreamHandler(stream=None)
+        self._metric_log.addHandler(ch2)
         self._metric_log.addHandler(logstash.LogstashHandler('192.168.1.2', 5002, version=1))
         self._metric_log.setLevel(logging.DEBUG)
         # self._metric_log.propagate = False
