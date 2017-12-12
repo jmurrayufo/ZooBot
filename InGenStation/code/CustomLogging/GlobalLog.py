243002ad6f5d348e22543b6b8df48c6e9b35c974
John Murray <jmurrayufo@gmail.com>
2017-12-11 23:19:42 -0700

    Push metric logs

--- InGenStation/code/CustomLogging/GlobalLog.py
+++ InGenStation/code/CustomLogging/GlobalLog.py
@@ -32,7 +32,7 @@ class Log:
         # self._metric_log.addHandler(ch2)
         self._metric_log.addHandler(logstash.LogstashHandler('192.168.1.2', 5002, version=1))
         self._metric_log.setLevel(logging.DEBUG)
-        self._metric_log.propagate = False
+        self._metric_log.propagate = True
 
 
     def __getattr__(self, name):
