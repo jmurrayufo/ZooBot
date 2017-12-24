99d4797cf3d2bb542005bdf6a3961806cac78504
John Murray <jmurrayufo@gmail.com>
2017-12-23 21:51:37 -0700

    Commiting the stub

---
+++ InGenStation/code/Devices/GPioStub.py
@@ -0,0 +1,25 @@
+
+
+class GPioStub:
+
+    BCM = None
+    OUT = None
+    LOW = None
+
+    @staticmethod
+    def setmode(*args, **kwargs):
+        pass
+
+
+    @staticmethod
+    def setup(*args, **kwargs):
+        pass
+
+        
+    @staticmethod
+    def output(*args, **kwargs):
+        pass
+
+    @staticmethod
+    def cleanup(*args, **kwargs):
+        pass
