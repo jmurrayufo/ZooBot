5c7bbd0c7ca137629930a6a6476fc6e3ccb1cb33
John Murray <jmurrayufo@gmail.com>
2018-01-03 22:24:28 -0700

    Print error as well

--- InGenStation/code/Devices/Dimmer.py
+++ InGenStation/code/Devices/Dimmer.py
@@ -134,6 +134,7 @@ class PID:
         """
 
         self.error = self.set_point - current_value
+        print(f"Error: {error}")
         if self.last_update is None:
             self.last_update = datetime.datetime.now()
             self.D_value = 0
