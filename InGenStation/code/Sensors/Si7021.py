b052217dba6d047009a9d67748363c72e716ab58
John Murray <jmurrayufo@gmail.com>
2018-02-02 01:01:58 -0700

    More agressive sleeps between Si7021 retries

--- InGenStation/code/Sensors/Si7021.py
+++ InGenStation/code/Sensors/Si7021.py
@@ -139,8 +139,9 @@ class Si7021:
                 if crc != list(read)[2]:
                     self.log.warning("CRC Error seen while reading the Si7021 sensor.")
                     self.log.warning(f"Values seen were {list(read)}, calculated crc was {crc}")
-                    time.sleep(0.01*loop)
+                    time.sleep(0.1 + 0.01*loop)
                     self.log.warning(f"Repolling sensor...")
+                    # Consider a reset here?
                     continue
                 break
             except OSError:
