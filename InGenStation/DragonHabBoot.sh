170feb6d9b1733e3bea00666c6da430c3973c93d
John Murray <jmurrayufo@gmail.com>
2018-02-19 12:27:36 -0700

    During dev, don't always pull pipenv

--- InGenStation/DragonHabBoot.sh
+++ InGenStation/DragonHabBoot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git pull
-pipenv uninstall --all
-pipenv install --skip-lock
+#pipenv uninstall --all
+#pipenv install --skip-lock
 pipenv run ./ZooBot.py --purpose dragon --update-delay 60
