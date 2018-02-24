a73882f514043e8dc0dfcbfa9a29c950dd302d9b
John Murray <jmurrayufo@gmail.com>
2018-02-24 12:39:36 -0700

    spelling error

--- Webcam/Webcam.py
+++ Webcam/Webcam.py
@@ -56,7 +56,7 @@ camera.push_settings()
 camera.log_settings()
 
 pygame.camera.init()
-pygame.camera.list_camera() #Camera detected or not
+pygame.camera.list_cameras() #Camera detected or not
 cam = pygame.camera.Camera("/dev/video0",(640,480))
 
 
