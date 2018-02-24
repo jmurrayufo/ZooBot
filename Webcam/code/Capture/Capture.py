b82956990d16ce913ae64598114e2a47a7164e4e
John Murray <jmurrayufo@gmail.com>
2018-02-24 01:13:04 -0700

    Better process protections

--- Webcam/code/Capture/Capture.py
+++ Webcam/code/Capture/Capture.py
@@ -16,20 +16,20 @@ class Capture:
         pass
 
     def run(self, file_name, size="1920x1080", quality=75):
-        self.log.debug(f"Run with filename {file_name}")
-        if not file_name.parent.exists():
-            self.log.info(f"Creating path {file_name.parent}")
-            os.makedirs(file_name.parent)
-
-        # Now capture an image
-        cmd = f"streamer -f jpeg -s1920x1080 -o {file_name}"
-        t0 = time.time()
         with DelayedKeyboardInterrupt():
+            self.log.debug(f"Run with filename {file_name}")
+            if not file_name.parent.exists():
+                self.log.info(f"Creating path {file_name.parent}")
+                os.makedirs(file_name.parent)
+
+            # Now capture an image
+            cmd = f"streamer -f jpeg -s1920x1080 -o {file_name}"
+            t0 = time.time()
             ps = subprocess.run(shlex.split(cmd), 
                                 stdout=subprocess.PIPE, 
                                 stderr=subprocess.PIPE)
-        t1 = time.time()
-        self.log.debug(f"Capture took {t1-t0}s. Max framerate is {1/(t1-t0)}fps")
+            t1 = time.time()
+            self.log.debug(f"Capture took {t1-t0}s. Max framerate is {1/(t1-t0)}fps")
 
 
 
