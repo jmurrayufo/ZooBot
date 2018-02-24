a1e10b7fe4dbae7657ba92aa723089f41fe61c71
John Murray <jmurrayufo@gmail.com>
2018-02-24 01:09:22 -0700

    Protect USB dropouts by completing captures

--- Webcam/code/Capture/Capture.py
+++ Webcam/code/Capture/Capture.py
@@ -3,6 +3,7 @@ import os
 import shlex
 import subprocess
 import time
+import signal
 
 from ..CustomLogging import Log
 
@@ -23,8 +24,27 @@ class Capture:
         # Now capture an image
         cmd = f"streamer -f jpeg -s1920x1080 -o {file_name}"
         t0 = time.time()
-        ps = subprocess.run(shlex.split(cmd), 
-                            stdout=subprocess.PIPE, 
-                            stderr=subprocess.PIPE)
+        with DelayedKeyboardInterrupt():
+            ps = subprocess.run(shlex.split(cmd), 
+                                stdout=subprocess.PIPE, 
+                                stderr=subprocess.PIPE)
         t1 = time.time()
         self.log.debug(f"Capture took {t1-t0}s. Max framerate is {1/(t1-t0)}fps")
+
+
+
+class DelayedKeyboardInterrupt(object):
+    def __enter__(self):
+        self.signal_received = False
+        self.old_handler = signal.signal(signal.SIGINT, self.handler)
+        self.log = Log()
+
+    def handler(self, sig, frame):
+        self.signal_received = (sig, frame)
+        self.log.critical('SIGINT received. Delaying KeyboardInterrupt.')
+
+    def __exit__(self, type, value, traceback):
+        signal.signal(signal.SIGINT, self.old_handler)
+        if self.signal_received:
+            self.log.critical("Now handling SIGINT")
+            self.old_handler(*self.signal_received)
