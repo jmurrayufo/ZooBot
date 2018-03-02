49b59c5287e91ae0107f91a511c387761693855e
John Murray <jmurrayufo@gmail.com>
2018-03-02 14:04:05 -0700

    Limit precisioun on reporting output value

--- InGenStation/code/Devices/Dimmer3.py
+++ InGenStation/code/Devices/Dimmer3.py
@@ -97,7 +97,7 @@ class Dimmer3:
 
     async def setOutput(self, channel, value):
         if (value > 100 or value < 0) and channel != 3:
-            self.log.warning(f"{self} saw setOutput value of {value}, outside range [0,100]!")
+            self.log.warning(f"{self} saw setOutput value of {value:.3f}, outside range [0,100]!")
 
         value = int(np.clip(100-value, 0, 100))
 
