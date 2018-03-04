c488feb66f6ab5b946630b6e81b1753024c35e62
John Murray <jmurrayufo@gmail.com>
2018-03-03 22:02:38 -0700

    Debug code enabled

--- InGenStation/code/Controllers/AstralController.py
+++ InGenStation/code/Controllers/AstralController.py
@@ -59,20 +59,20 @@ class AstralController(Controller):
             sunset = (sun['sunset'] + offset).replace(tzinfo=None)
             dusk = (sun['dusk'] + offset).replace(tzinfo=None)
 
-            if False and datetime.date.today() < datetime.date(2018,6,20):
+            if True and datetime.date.today() < datetime.date(2018,6,20):
                 # Overide for baby dragon
                 if datetime.date.today() != self.today:
                     self.log.info("New dragon overide protocall is in effect.")
 
-                dawn = datetime.datetime.now().replace(hour=6, minute=16, second=00)
+                dawn = datetime.datetime.now().replace(hour=6, minute=16, second=0)
                 sunrise = datetime.datetime.now().replace(hour=6, minute=45, second=54)
                 sunset = datetime.datetime.now().replace(hour=21, minute=18, second=15)
                 dusk = datetime.datetime.now().replace(hour=21, minute=48, second=9)
                 
-                dawn = datetime.datetime.now().replace(hour=8, minute=16, second=00)
-                sunrise = datetime.datetime.now().replace(hour=8, minute=45, second=00)
-                sunset = datetime.datetime.now().replace(hour=21, minute=00, second=15)
-                dusk = datetime.datetime.now().replace(hour=21, minute=30, second=9)
+                dawn = datetime.datetime.now().replace(hour=22, minute=5, second=0)
+                sunrise = datetime.datetime.now().replace(hour=22, minute=25, second=0)
+                sunset = datetime.datetime.now().replace(hour=22, minute=30, second=0)
+                dusk = datetime.datetime.now().replace(hour=22, minute=50, second=0)
 
             if datetime.date.today() != self.today:
                 self.log.info("Todays times are as follows!")
