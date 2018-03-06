3ce135d56b20b81a503a82ef2d979eb44a0735aa
John Murray <jmurrayufo@gmail.com>
2018-03-06 08:12:40 -0700

    Adjust Back down basking area

--- InGenStation/code/DragonHab.py
+++ InGenStation/code/DragonHab.py
@@ -29,7 +29,7 @@ class DragonHab:
         self.devices['dimmer0'] = Dimmer3("dimmer0", 0x3F, args) 
 
         tmp_controller = AstralController(args, 'astr0', "35°18'N", "105°06'W",  
-            elivation=0, day_value=44, night_value=0, report_times=True)
+            elivation=0, day_value=42, night_value=0, report_times=True)
         self.devices['dimmer0'].bind(tmp_controller, 1)
         # Commented out until we get proper venting of the vivarium
         self.devices['dimmer0'].bind(tmp_controller, 4, override=43)
