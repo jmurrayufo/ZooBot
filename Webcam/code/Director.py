c0644162b0ca085bbebc0bfb5956d16d02e0e429
John Murray <jmurrayufo@gmail.com>
2018-03-08 00:40:31 -0700

    Comments for how to run Director

--- Webcam/code/Director.py
+++ Webcam/code/Director.py
@@ -13,4 +13,16 @@ class Director:
 
     def run(self):
         while 1:
+            # Determine if we need to throttle back
+
+            # Wait perscribed time
+
+            # Capture image
+
+            # Write image and manifest to Ramdisk
+
+            # Check for copied files to delete
+
+            # Clean up Ramdisk as able
+            
             pass
