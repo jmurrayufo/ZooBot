ae8b439aa724e02f4f98b64393e97573ab526d8a
John Murray <jmurrayufo@gmail.com>
2018-03-09 14:37:50 -0700

    15 more minutes of offset for sunrise/set

--- InGenStation/code/Controllers/AstralController.py
+++ InGenStation/code/Controllers/AstralController.py
@@ -58,10 +58,10 @@ class AstralController(Controller):
 
             sun = l.sun(date=datetime.date.today())
 
-            dawn = (sun['dawn'] + offset).replace(tzinfo=None) - datetime.timedelta(minutes=45)
+            dawn = (sun['dawn'] + offset).replace(tzinfo=None) - datetime.timedelta(minutes=60)
             sunrise = (sun['sunrise'] + offset).replace(tzinfo=None)
             sunset = (sun['sunset'] + offset).replace(tzinfo=None)
-            dusk = (sun['dusk'] + offset).replace(tzinfo=None) + datetime.timedelta(minutes=45)
+            dusk = (sun['dusk'] + offset).replace(tzinfo=None) + datetime.timedelta(minutes=60)
 
             if False and datetime.date.today() < datetime.date(2018,6,20):
                 # Overide for baby dragon
