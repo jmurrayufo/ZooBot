24343debb0229b747c5d81b99a2ee4583cd76b4c
John Murray <jmurrayufo@gmail.com>
2018-03-13 18:02:38 -0600

    Dont uninstall everything on Roachhab reboot

--- InGenStation/RoachHabBoot.sh
+++ InGenStation/RoachHabBoot.sh
@@ -3,6 +3,6 @@
 # Loop forever unless we can talk to google
 until ping -c1 www.google.com &>/dev/null; do :; done
 git pull
-pipenv uninstall --all
-pipenv install --skip-lock
+# pipenv uninstall --all
+# pipenv install --skip-lock
 pipenv run ./ZooBot.py --purpose bug
