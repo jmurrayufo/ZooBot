d78ba6decb5d45d937ae4a2c3e1031c261f5c91c
John Murray <jmurrayufo@gmail.com>
2018-03-13 19:58:31 -0600

    Mode debug

--- InGenStation/code/Sensors/Si7021.py
+++ InGenStation/code/Sensors/Si7021.py
@@ -150,6 +150,7 @@ class Si7021:
         with I2C(address=self.address) as i2c:
             i2c.write_byte(self.MEASURE_TEMPERATURE_NO_HOLD)
             count, data = i2c.read_bytes(2)
+            self.log.debug(count)
         return data[0] << 8 + data[1]
 
 
