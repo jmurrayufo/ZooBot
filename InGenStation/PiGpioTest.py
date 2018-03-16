a56f18d55c4df6ef3be8dc8aee64a77bc9061627
John Murray <jmurrayufo@gmail.com>
2018-03-16 00:36:33 -0600

    Loop Testing

--- InGenStation/PiGpioTest.py
+++ InGenStation/PiGpioTest.py
@@ -4,14 +4,17 @@ import pigpio
 import time
 
 from code.I2C import I2C2
-
-while 1:
-    time.sleep(0)
-    print()
-    with I2C2(0x40) as i2c:
-        count, data = i2c.Si7021_humidity()
-        if count<0:
-            print(pigpio.error_text(count))
-        else:
-            for i in data:
-                print(i)
+t1 = time.time()
+loops = 0
+try:
+    while 1:
+        loops += 1
+        with I2C2(0x40) as i2c:
+            count, data = i2c.Si7021_humidity()
+            if count<0:
+                raise IOError
+finally:
+    t2 = time.time()
+    print(t2-t1)
+    print(loops)
+    print(loops/(t2-t1))
