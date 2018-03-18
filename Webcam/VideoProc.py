9e32bdba0d203c741060b55b6a870e84df045d7a
John Murray <jmurrayufo@gmail.com>
2018-03-17 19:50:13 -0600

    VideoProc completed and ready for use

--- Webcam/VideoProc.py
+++ Webcam/VideoProc.py
@@ -6,7 +6,6 @@ import datetime
 import json
 import os
 import shlex
-import shutil
 import subprocess
 import time
 
@@ -27,6 +26,14 @@ parser.add_argument('--videos',
                     default="/ZFS/Media/Webcams/Dragonhab/Videos",
                     help='Output directory of videos')
 
+parser.add_argument('--fps',
+                    default=30,
+                    help="FPS of output video files")
+
+parser.add_argument('--now',
+                    action='store_true',
+                    help="Process files now")
+
 args = parser.parse_args()
 today = datetime.date.today()
 movie_process = None
@@ -35,6 +42,13 @@ log = Log("VideoProc.py", args)
 log.info("Started")
 
 next_scan = datetime.datetime.now()
+if args.now:
+    log.info("Processing queue right away")
+else:
+    log.info("Processing will start at the next 1AM")
+    next_scan += datetime.timedelta(days=1)
+    next_scan = next_scan.replace(hour=1, minute=0, second=0, microsecond=0)
+
 
 while 1:
     # Scan for new directories without a manifest.json file
@@ -42,7 +56,7 @@ while 1:
     now = datetime.datetime.now()
     if now < next_scan:
         dt = (next_scan-now).total_seconds()
-        log.info(f"Sleep for {dt:,.0f} seconds")
+        log.info(f"Sleep for {dt:,.0f} seconds until {next_scan}")
         time.sleep(dt)
 
     next_scan = datetime.datetime.now()
@@ -66,11 +80,28 @@ while 1:
         if list(folder.glob("*.jpeg")):
             log.debug("Found jpeg without manifest file!")
             targets.append(Video(folder, args))
-            break
             continue
 
     for video in targets:
         video.load()
-        # log.debug(video.__dict__)
 
-    break
+    ffmpeg_processes = []
+    for video in targets:
+        if video.processed:
+            log.info(f"{video} is aleady finished processing! Delete?")
+        ffmpeg_processes.append(video)
+
+    log.info(f"Started videos, processing {len(ffmpeg_processes)}")
+    log.info("Wait until completed")
+
+    while len(ffmpeg_processes):
+
+        video = ffmpeg_processes[0]
+        video.start()
+        log.info(f"{video} Processing has started.")
+        while video.is_running():
+            time.sleep(1)
+        video.finished()
+        log.info(f"{video} is completed.")
+
+        ffmpeg_processes = [x for x in ffmpeg_processes if not x.processed]
--- Webcam/code/Video.py
+++ Webcam/code/Video.py
@@ -1,7 +1,10 @@
 
 from pathlib import Path
-import re
 import json
+import re
+import time
+import subprocess
+import shlex
 
 from .CustomLogging import Log
 
@@ -21,33 +24,85 @@ class Video:
         self.manifest_file = None
 
 
+    def __repr__(self):
+        return f"Video({self.target_folder})"
+
+
     def load(self):
 
         manifest_file = list(self.target_folder.glob("*manifest.json"))
         if manifest_file:
-            with open(manifest_file[0],'r') as fp:
-                self.__dict__.update(json.load(fp))
-                self.target_folder = Path(self.target_folder)
-                self.output_file = Path(self.output_file)
-                self.manifest_file = Path(self.manifest_file)
+            try: 
+                with open(manifest_file[0],'r') as fp:
+                    self.__dict__.update(json.load(fp))
+                    self.target_folder = Path(self.target_folder)
+                    self.output_file = Path(self.output_file)
+                    self.manifest_file = Path(self.manifest_file)
+                    return
+            except json.decoder.JSONDecodeError:
+                self.log.exception("JSON was corrupted somehow, creating a new one")
 
-        else:
-            self.log.info("No manifest found, generate one now!")
 
-            match_obj = re.search("(\d{4})/(\d{2})/(\d{2})", str(self.target_folder))
-            year, month, day = match_obj.groups()
-            self.output_file = Path(self.args.videos, f"{year}_{month}_{day}.mp4")
-            self.manifest_file = Path(self.target_folder, f"{year}_{month}_{day}_manifest.json")
+        self.log.info("No manifest found, generate one now!")
 
-            data = dict(self.__dict__)
-            del data['log']
-            del data['args']
-            data['target_folder'] = str(data['target_folder'])
-            data['output_file'] = str(data['output_file'])
-            data['manifest_file'] = str(data['manifest_file'])
+        match_obj = re.search("(\d{4})/(\d{2})/(\d{2})", str(self.target_folder))
+        year, month, day = match_obj.groups()
+        self.output_file = Path(self.args.videos, f"{year}_{month}_{day}.mp4")
+        self.manifest_file = Path(self.target_folder, f"{year}_{month}_{day}_manifest.json")
 
-            with open(self.manifest_file,'w') as fp:
-                json.dump(data, fp)
+        data = dict(self.__dict__)
+        del data['log']
+        del data['args']
+        data['target_folder'] = str(data['target_folder'])
+        data['output_file'] = str(data['output_file'])
+        data['manifest_file'] = str(data['manifest_file'])
+
+        with open(self.manifest_file,'w') as fp:
+            json.dump(data, fp, indent=2)
+
+    def start(self):
+        if self.process is not None:
+            # Check to see if we are done, mark this as done, and return
+            self.log.critical("Process already started!")
+            return
+
+        # Check to see if we are old enough (min 1 hours)
+        # Loop through all JPEGs 
+        for image in self.target_folder.glob("*.jpeg"):
+            if time.time() - image.stat().st_mtime < 1*60*60:
+                self.log.info(f"Skipping, {image} is only {time.time() - image.stat().st_mtime} old")
+                return
+
+        # Kickoff process to compress this
+
+        cmd = f"ffmpeg -y -r 30 -pattern_type glob -i '{str(self.target_folder)}/*.jpeg' -r 30 {str(self.output_file)}"
+        self.log.debug(cmd)
+        self.process = subprocess.Popen(shlex.split(cmd), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    def is_running(self):
+        if self.process is None:
+            self.log.critical("Cannot update a process that isn't running!")
+            return False
+
+        ret = self.process.poll()
+        return ret is None
+
+
+    def finished(self):
+        # Mark json as completed, and not needing another update
+
+        self.processed = True
+        self.process = None
+
+        data = dict(self.__dict__)
+        del data['log']
+        del data['args']
+        data['target_folder'] = str(data['target_folder'])
+        data['output_file'] = str(data['output_file'])
+        data['manifest_file'] = str(data['manifest_file'])
+
+        with open(self.manifest_file,'w') as fp:
+            json.dump(data, fp, indent=2)
 
 
 # class CustomEncoder(json.JSONEncoder):
