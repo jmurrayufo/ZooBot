90cd03798dab1b09a3311cdc3a749bb7878c9ace
John Murray <jmurrayufo@gmail.com>
2018-03-24 15:02:16 -0600

    Up basking lamp again

--- InGenStation/code/DragonHab.py
+++ InGenStation/code/DragonHab.py
@@ -23,7 +23,7 @@ class DragonHab:
         Integrator_min = -30
 
         # Basking Lamp and UV Config
-        basking_lamp_setting = 50
+        basking_lamp_setting = 54
         uv_trigger = basking_lamp_setting - 1
 
         # Ambient Temperatures
